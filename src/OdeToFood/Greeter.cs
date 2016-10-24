4bfcaae13dc91cfbab57a5b8361cc8c489ada42e
Matt Wagner <m.wagner@dwdiesel.com>
2016-10-24 10:05:55 -0400

    Create a Greeting service
    Create a Greeting service that uses an interface to get the greeting
    from a json file, rather than accessing the file directly in the
    Configure method in Startup.cs.

---
+++ src/OdeToFood/Greeter.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.Configuration;
+
+namespace OdeToFood
+{
+    public interface IGreeter
+    {
+        string GetGreeting();
+    }
+
+    public class Greeter : IGreeter
+    {
+        private string _greeting;
+
+        public Greeter(IConfiguration configuration)
+        {
+            _greeting = configuration["Greeting"];
+        }
+
+        public string GetGreeting()
+        {
+            return _greeting;
+        }
+    }
+}
