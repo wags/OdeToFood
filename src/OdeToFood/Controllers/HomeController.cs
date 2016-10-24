a892427c68bd2cededbd07cb3d38eed1de3afb90
Matt Wagner <m.wagner@dwdiesel.com>
2016-10-24 12:25:29 -0400

    Set up an Action Result
    Configure HomeController to subclass Controller, and return an
    IActionResult from the Index method. Returning an ObjectResult defaults
    to a JSON representation of the model object.

--- src/OdeToFood/Controllers/HomeController.cs
+++ src/OdeToFood/Controllers/HomeController.cs
@@ -1,10 +1,15 @@
-﻿namespace OdeToFood.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Models;
+
+namespace OdeToFood.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public IActionResult Index()
         {
-            return "Hello from the HomeController";
+            var model = new Restaurant { Id = 1, Name = "The House of Kobe" };
+
+            return new ObjectResult(model);
         }
     }
 }
