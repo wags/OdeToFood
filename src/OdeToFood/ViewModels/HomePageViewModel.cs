5051939b81c3857fce7309bcdbe8f5b9cae00f70
Matt Wagner <m.wagner@dwdiesel.com>
2016-10-24 14:35:25 -0400

    Add a view model to hold data from two different services
    A view model is used when data from multiple sources needs to be passed
    to a view. In this case, a view model with a greeting and the list of
    restaurants is set up and passed to the home view.

---
+++ src/OdeToFood/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using OdeToFood.Entities;
+
+namespace OdeToFood.ViewModels
+{
+    public class HomePageViewModel
+    {
+        public string CurrentMessage { get; set; }
+        public IEnumerable<Restaurant> Restaurants { get; set; }
+    }
+}
