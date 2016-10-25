9bbbcfb7b8c8204d06a9f177a738ced57b0543e8
Matt Wagner <m.wagner@dwdiesel.com>
2016-10-25 16:22:46 -0400

    Demonstrate using a view component for the greeting

---
+++ src/OdeToFood/ViewComponents/GreetingViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using OdeToFood.Services;
+
+namespace OdeToFood.ViewComponents
+{
+    public class GreetingViewComponent : ViewComponent
+    {
+        private IGreeter _greeter;
+
+        public GreetingViewComponent(IGreeter greeter)
+        {
+            _greeter = greeter;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var model = _greeter.GetGreeting();
+            return View("Default", model);
+        }
+    }
+}
