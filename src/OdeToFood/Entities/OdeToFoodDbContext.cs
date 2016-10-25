2446ef9831bda277437163982543138138aceb84
Matt Wagner <m.wagner@dwdiesel.com>
2016-10-25 08:35:53 -0400

    Implement the DbContext
    Add an implementation that uses SQL server.

---
+++ src/OdeToFood/Entities/OdeToFoodDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OdeToFood.Entities
+{
+    public class OdeToFoodDbContext : DbContext
+    {
+        public OdeToFoodDbContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Restaurant> Restaurants { get; set; }
+    }
+}
